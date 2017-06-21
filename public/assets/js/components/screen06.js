62fce32d9bebb822c4f2d1ece030aaf9d723e021
Dana Franco <turquoise.ilg@hotmail.com>
2017-06-21 18:39:23 -0500

    maquetando screen06

--- public/assets/js/components/screen06.js
+++ public/assets/js/components/screen06.js
@@ -2,17 +2,19 @@
 
 const ScreenSix = (update) =>{
 
-   const section = $('<section>');
-   const divImg  = $('<div>',{class:'row'});
-   const img     = $('<img>',{src:'img/icons/bcp-logo.png',class:'col s4 offset-s4'});
-   const divText6 = $('<div>',{class:'row'});
-   const h2      = $('<h2>',{text:'Registra tu tarjeta débito BCP'});
-   const p       = $('<p>',{text:'Por ahora solo aceptamos cuentas de ahorro y/o corriente en soles.'})
+   const section  = $('<section>');
+   const img1     = $('<img>',{src:'img/icons/question.png',class:'col s2 offset-s10 img-icon6'});
+   const divImg6  = $('<div>',{class:'row divImg6'});
+   const img2     = $('<img>',{src:'img/icons/bcp-logo.png',class:'col s4 offset-s4 img2'});
+   const divText6 = $('<div>',{class:'row divText6'});
+   const h2       = $('<h2>',{text:'Registra tu tarjeta débito BCP',class:'col s12 col m4 offset-m4'});
+   const p        = $('<p>',{text:'Por ahora solo aceptamos cuentas de ahorro y/o corriente en soles.',class:'col s12'});
 
-   divImg.append(img);
+   divImg6.append(img1);
+   divImg6.append(img2);
    divText6.append(h2);
    divText6.append(p);
-   section.append(divImg);
+   section.append(divImg6);
    section.append(divText6);
 
   return section
