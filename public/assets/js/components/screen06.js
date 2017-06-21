847335322fb342abd01d619871132e4cfb52a64b
Dana Franco <turquoise.ilg@hotmail.com>
2017-06-21 17:48:25 -0500

    hito 2

--- public/assets/js/components/screen06.js
+++ public/assets/js/components/screen06.js
@@ -6,7 +6,7 @@ const ScreenSix = (update) =>{
    const divImg  = $('<div>',{class:'row'});
    const img     = $('<img>',{src:'img/icons/bcp-logo.png',class:'col s4 offset-s4'});
    const divText6 = $('<div>',{class:'row'});
-   const h2      = $('<div>',{text:'Registra tu tarjeta débito BCP'});
+   const h2      = $('<h2>',{text:'Registra tu tarjeta débito BCP'});
    const p       = $('<p>',{text:'Por ahora solo aceptamos cuentas de ahorro y/o corriente en soles.'})
 
    divImg.append(img);
